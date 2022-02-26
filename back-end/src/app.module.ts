6bb8957d13333a3abc7ba5f84ac84dcb8f13f734
Hamza <hamzaelkhatri@gmail.com>
2022-02-26 16:46:23 +0100

    fix:typeorm conflit with postgres

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,13 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AuthModuleOptions } from '@nestjs/passport';
-import { UsersModule } from './users/users.module';
 import { AppGateway } from './app.gateway';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
 
 @Module({
-  imports: [AuthModule,UsersModule,AuthModuleOptions,ConfigModule.forRoot()],
-//   imports: []
+  imports: [AuthModule,AuthModuleOptions,ConfigModule.forRoot(),TypeOrmModule.forRoot(
+    {
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities:[User],
+      synchronize: true,
+    }
+  ), UserModule],
   controllers: [AppController],
   providers: [AppService,AuthModuleOptions,AppGateway],
 })
--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor() {}
 
   async validateUser(token: string): Promise<any> {
     // console.log(username);
