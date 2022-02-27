0b0674ffcb30eb5d47dd8acf27a6324eda6fe323
Hamza <hamzaelkhatri@gmail.com>
2022-02-27 19:32:02 +0100

    feat: link between api42 and database

--- back-end/src/auth/auth.module.ts
+++ back-end/src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { AuthModuleOptions } from '@nestjs/passport';
+import { UserModule } from '../user/user.module';
+
 
 
 @Module({
-  imports: [PassportModule,AuthModuleOptions],
+  imports: [PassportModule,AuthModuleOptions,UserModule],
   providers: [AuthService, LocalStrategy],
   exports : [AuthService],
 })
--- back-end/src/user/user.service.ts
+++ back-end/src/user/user.service.ts
@@ -2,11 +2,43 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { User } from './user.entity';
+import { CrudRequest } from '@nestjsx/crud';
 
 @Injectable()
-export class UserService extends TypeOrmCrudService<User> {
+export default class UserService extends TypeOrmCrudService<User> 
+{
+
+    repository: any;
     constructor(@InjectRepository(User) repository) {
         super(repository);
+        this.repository = repository;
     }
+
+    async createOne(request: CrudRequest, data: Partial<User>) 
+    {
+        this.findByEmail(data.email).then(user => {
+            if (user) {
+                throw new Error('User already exists');
+            }
+        });
+        
+        const user = new User();
+        user.name = data.name;
+        user.email = data.email;
+        user.image = data.image;
+        user.is_online = data.is_online;
+        user.token = data.token;
+        user.created_at = data.created_at;
+        user.updated_at = data.updated_at;
+        user.deleted_at = data.deleted_at;
+        user.is_verified = data.is_verified;
+        return await this.repository.save(user);
+    }
+
+    findByEmail(email: string) 
+    {
+        return this.repository.findOne({ email: email });
+    }
+
 }
 
