fcaeb5efe65cc2d03943a4e6fcbca970b3e7c188
Hamza <hamzaelkhatri@gmail.com>
2022-03-20 17:13:23 +0100

    Feat: add random players matchmaking

--- front-end/pages/First_pages.tsx
+++ front-end/pages/First_pages.tsx
@@ -4,13 +4,13 @@ import { useRef, useEffect } from "react";
 import { useState } from "react";
 import _Canvas from "./page1";
 import "antd/dist/antd.css";
-import { Card, Avatar, Badge, Result } from "antd";
+import { Card, Avatar, Badge, Result, Row, Col } from "antd";
 import { ArrowLeftOutlined, PlayCircleOutlined, ArrowRightOutlined, DislikeOutlined, FlagOutlined, LikeOutlined, FieldNumberOutlined, EnvironmentOutlined, InfoCircleFilled } from "@ant-design/icons";
 import Canvas from "./Game";
 import axios from "axios";
 import MatchLive from "./live_match";
 import Leaderboard from "./leaderboard";
-import {MyProvider, useMyContext} from "./ContextProvider";
+import { MyProvider, useMyContext } from "./ContextProvider";
 const { Meta } = Card;
 
 import { Button, notification, Carousel } from 'antd';
@@ -37,7 +37,7 @@ const Next_page = () => {
 
     let context = useMyContext();
 
-    
+
     const close = (key: string) => {
         axios.get("http://10.12.8.15:3000/game/invited/reject/" + localStorage.getItem("id") + "/" + context.ShowCanvas.gameInfo['id']).then(res => {
             notification.close(key);
@@ -48,8 +48,8 @@ const Next_page = () => {
         console.log(context.ShowCanvas.gameInfo);
 
         axios.get("http://10.12.8.15:3000/game/invited/confirm/" + localStorage.getItem("id") + '/' + context.ShowCanvas.gameInfo['id']).then(res => {
-        console.log(res.data);    
-        context.setShowCanvas(
+            console.log(res.data);
+            context.setShowCanvas(
                 {
                     gameInfo: res.data,
                     show: true
@@ -72,7 +72,7 @@ const Next_page = () => {
             </div>
         );
         notification.open({
-            message:' invited you to play a game',
+            message: ' invited you to play a game',
             description:
                 'Do you want to play with ge:',
             btn,
@@ -84,15 +84,13 @@ const Next_page = () => {
             // onClose: close,
         });
     };
-    useEffect(() => 
-    {
+    useEffect(() => {
         const inter = setInterval(() => {
             if (oneTime1 == 0) {
 
                 axios.get("http://10.12.8.15:3000/game/is_invited/" + localStorage.getItem("id"))
                     .then(res => {
-                        if (res.data['id'] !== undefined)
-                         {
+                        if (res.data['id'] !== undefined) {
                             setOneTime1(1);
                             context.setShowCanvas(
                                 {
@@ -100,7 +98,7 @@ const Next_page = () => {
                                     gameInfo: res.data
                                 }
                             );
-                            console.log('here',context.ShowCanvas.gameInfo);
+                            console.log('here', context.ShowCanvas.gameInfo);
                             context.ShowCanvas.gameInfo = res.data;
                             openNotification(res.data);
                             clearInterval(inter);
@@ -117,96 +115,98 @@ const Next_page = () => {
             if (oneTime === 0) {
                 setData(res.data);
                 setOneTime(1);
-                
+
             }
         });
     return (
-        <div className="ant-row">
-            <div className="ant-col ant-col-xs-28 ant-col-xl-24" style={{ top: "150px" }}>
-                {!context.ShowCanvas.show &&
-                    <Card
-                        style={{ padding: "1%", width: "20%", height: "auto", left: "20%", top: "15%", position: "absolute", zIndex: "2", borderRadius: "3%", background: "white" }}
-                        cover={
-                      
-                            <center>
-                                <Badge.Ribbon text="online" style={{ backgroundColor: '#87d068' }} placement='start' />
-                                <Avatar shape="square" size={200} src="https://joeschmoe.io/api/v1/random" />
-                            </center>
-                        }
-                        actions={[<ArrowLeftOutlined key="previous" onClick={() => { setOneTime(0); }
-                        } />,
-                        <PlayCircleOutlined key="play" onClick={() => {
-                            axios.post("http://10.12.8.15:3000/game/invite",
-                            {
-                                "username1": localStorage.getItem("usual_full_name"),
-                                "username2": data['name']
-                            })
-                            .then(res => {
-                                // if (oneTime === 0) {
-                                    if (res.data.length !== 0) {
-                                    setData(res.data);
-                                    context.setShowCanvas(
-                                        {
-                                            show: true,
-                                            gameInfo: res.data
-                                        }
-                                    )
-                                    setOneTime(1);
-                                    }
-                                });
-                        }} />,
-                        <ArrowRightOutlined key="next" onClick={() => {
-                            setOneTime(0);
-                        }} />
-                        ]}
-                    >
-                        <Meta
-                            title={data['name']}
-                            description={
-                                <ul>
-                                    <i id="icons">
-                                        <li>
-                                            <EnvironmentOutlined /> :  {data['country']}
-                                        </li>
-                                        <li>
-                                            <FieldNumberOutlined /> : Level {data['level']}
-                                        </li>
-                                        <li>
-                                            <LikeOutlined /> : wins {data['wins']} Matchs
-                                        </li>
+        <div>
+            <div className="ant-row">
+                <div className="ant-col ant-col-xs-28 ant-col-xl-24" style={{ top: "150px" }}>
+                    {!context.ShowCanvas.show &&
+                        <Card
+                            style={{ padding: "1%", width: "20%", height: "auto", left: "20%", top: "15%", position: "absolute", zIndex: "2", borderRadius: "3%", background: "white" }}
+                            cover={
 
-                                        <li>
-                                            <DislikeOutlined /> : lost {data['loses']} Matchs
-                                        </li>
-                                        <li>
-                                            <FlagOutlined /> : Quit {data['quit']} Match
-                                        </li>
-
-                                    </i>
-
-
-                                </ul>
+                                <center>
+                                    <Badge.Ribbon text="online" style={{ backgroundColor: '#87d068' }} placement='start' />
+                                    <Avatar shape="square" size={200} src="https://joeschmoe.io/api/v1/random" />
+                                </center>
                             }
-                        />
-                    </Card>
-                }
-            </div>
-            <div className="ant-col ant-col-xs-28 ant-col-xl-24" style={{ top: "150px"  }}>
-                {!context.ShowCanvas.show &&
-                    <div style={{ position: "absolute", top: "15%", left: "50%", zIndex: "2" ,width:"40%" , height:"auto"}}>
-                        <MatchLive />
-                    </div>
-                }
-            </div>
-            <div className="ant-col ant-col-xs-28 ant-col-xl-24"  style={{ top: "1000px", width:"100%"  , left: "25%", zIndex: "2" }} >
-                {!context.ShowCanvas.show &&
-                    <div >
-                        {/* <Leaderboard /> */}
-                    </div>
-                }
-            </div>
+                            actions={[<ArrowLeftOutlined key="previous" onClick={() => { setOneTime(0); }
+                            } />,
+                            <PlayCircleOutlined key="play" onClick={() => {
+                                axios.post("http://10.12.8.15:3000/game/invite",
+                                    {
+                                        "username1": localStorage.getItem("usual_full_name"),
+                                        "username2": data['name']
+                                    })
+                                    .then(res => {
+                                        // if (oneTime === 0) {
+                                        if (res.data.length !== 0) {
+                                            setData(res.data);
+                                            context.setShowCanvas(
+                                                {
+                                                    show: true,
+                                                    gameInfo: res.data
+                                                }
+                                            )
+                                            setOneTime(1);
+                                        }
+                                    });
+                            }} />,
+                            <ArrowRightOutlined key="next" onClick={() => {
+                                setOneTime(0);
+                            }} />
+                            ]}
+                        >
+                            <Meta
+                                title={data['name']}
+                                description={
+                                    <ul>
+                                        <i id="icons">
+                                            <li>
+                                                <EnvironmentOutlined /> :  {data['country']}
+                                            </li>
+                                            <li>
+                                                <FieldNumberOutlined /> : Level {data['level']}
+                                            </li>
+                                            <li>
+                                                <LikeOutlined /> : wins {data['wins']} Matchs
+                                            </li>
 
-            {/* {ShowCanvas && <Carousel
+                                            <li>
+                                                <DislikeOutlined /> : lost {data['loses']} Matchs
+                                            </li>
+                                            <li>
+                                                <FlagOutlined /> : Quit {data['quit']} Match
+                                            </li>
+
+                                        </i>
+
+
+                                    </ul>
+                                }
+                            />
+                        </Card>
+                    }
+                </div>
+                <div className="ant-col ant-col-xs-28 ant-col-xl-24" style={{ top: "150px" }}>
+                    {!context.ShowCanvas.show &&
+                        <div style={{ position: "absolute", top: "15%", left: "50%", zIndex: "2", width: "40%", height: "auto" }}>
+                            <MatchLive />
+                        </div>
+                    }
+                </div>
+                <div className="ant-col ant-col-xs-28 ant-col-xl-24" style={{ top: "1000px", width: "100%", left: "25%", zIndex: "2" }} >
+                    {!context.ShowCanvas.show &&
+                        <div >
+                            {/* <Leaderboard /> */}
+                        </div>
+                    }
+                </div>
+
+
+                {/* {ShowCanvas && <Carousel
                 beforeChange={(current) => {
                     if (current === 3) {
                         setShowCanvas(true);
@@ -245,27 +245,52 @@ const Next_page = () => {
                     <h3 style={contentStyle}>Good Luck !<HeartOutlined />  </h3>
                 </div>
             </Carousel >} */}
-            {
-                //make Canvas in center of the screen
-                <div className="ant-col ant-col-xs-28 ant-col-xl-24" style={{ top: "20%", position: "absolute", zIndex: "2", left: "50%", transform: "translate(-50%,0)" }}>
-                    {context.ShowCanvas.show && <Canvas data={context.ShowCanvas['gameInfo']} />}
-                </div>
-            }
+                {
+                    //make Canvas in center of the screen
+                    <div className="ant-col ant-col-xs-28 ant-col-xl-24" style={{ top: "20%", position: "absolute", zIndex: "2", left: "50%", transform: "translate(-50%,0)" }}>
+                        {context.ShowCanvas.show && <Canvas data={context.ShowCanvas['gameInfo']} />}
+                    </div>
+                }
 
-            {
-            //       <Result style={{ top: "20%", position: "absolute", zIndex: "2", left: "50%", transform: "translate(-50%,0)" }}
-            //       status="success"
-            //       title="Successfully Purchased Cloud Server ECS!"
-            //       subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
-            //       extra={[
-            //           <Button type="primary" key="console">
-            //               Go Console
-            //           </Button>,
-            //           <Button key="buy">Buy Again</Button>,
-            //       ]}
-            //   />
-            }
-        </div>
+                {
+                    //       <Result style={{ top: "20%", position: "absolute", zIndex: "2", left: "50%", transform: "translate(-50%,0)" }}
+                    //       status="success"
+                    //       title="Successfully Purchased Cloud Server ECS!"
+                    //       subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
+                    //       extra={[
+                    //           <Button type="primary" key="console">
+                    //               Go Console
+                    //           </Button>,
+                    //           <Button key="buy">Buy Again</Button>,
+                    //       ]}
+                    //   />
+                }
+            </div>
+
+            <Row>
+                <Col span={1} offset={12} >
+                    <Button type="primary" style={{zIndex: "9999" , top: "25%"}} onClick={() => {
+                        axios.get("http://localhost:3000/game/matchmaking/"+localStorage.getItem("id"))
+                        .then(res => 
+                            {
+                                if (res.data.length !== 0) 
+                                {
+                                    setData(res.data);
+                                    context.setShowCanvas(
+                                        {
+                                            show: true,
+                                            gameInfo: res.data
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    }}>
+                        Random Match
+                    </Button>
+                </Col>
+            </Row>
+        </div >
     )
 }
 
