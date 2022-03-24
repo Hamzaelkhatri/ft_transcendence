16a782b5db9a4758aba105e4bda4ea30bc2a71b0
hamza <hamzaelkhatri@gmail.com>
2022-03-24 22:13:31 +0100

    fix:number of lose & wins

--- back-end/src/game/game.service.ts
+++ back-end/src/game/game.service.ts
@@ -162,18 +162,18 @@ export class GameService extends TypeOrmCrudService<Game>
             .where('game.id = :id', { id: id })
             .getOne();
         const users = await this.userservice.repository.findOne({ id: winner });
-        users.wins++;
+        users.wins = users.wins + 1;
         await this.userservice.repository.save(users);
         // console.log(game);
         if (game.user1.id !== winner) {
             let user:any = await this.userservice.repository.findOne({ id: game.user1.id });
-            user.loses++;
+            user.loses = user.loses + 1;
             await this.userservice.repository.save(user);
 
         }
         else {
             let user:any = await this.userservice.repository.findOne({ id: game.user2.id });
-            user.loses++;
+            user.loses = user.loses + 1;
             await this.userservice.repository.save(user);
 
         }
