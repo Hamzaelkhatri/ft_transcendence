90690268c5b13e5bd256cdc534c82dbbd27a93c1
zdnaya <zdnaya@student.1337.ma>
2022-04-14 23:04:04 +0000

    2022-04-14 23:04:04

--- back-end/dist/app.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-export declare class AppController {
-    router: Router;
-    login(res: any, req: any): Promise<void>;
-    wlcome(): Promise<string>;
-}
--- back-end/dist/auth/auth.service.d.ts
+++
@@ -1,7 +0,0 @@
-import UserService from '../user/user.service';
-import { User } from 'src/user/user.entity';
-export declare class AuthService {
-    userService: UserService;
-    constructor(userService: UserService);
-    validateUser(token: any, req: any): Promise<User>;
-}
